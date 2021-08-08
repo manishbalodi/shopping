16a36260ecd46b4ccbe799a295d5e1b60707bec0
manishbalodi <manish.balodi.1998@gmail.com>
2021-08-09 00:16:08 +0530

    execpt list and detail component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ElectronicComponent } from './electronic/electronic.component';
+import { LandingComponent } from './electronic/landing/landing.component';
+import { MobileComponent } from './electronic/mobile/mobile.component';
+import { FashionComponent } from './fashion/fashion.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo :'/dashboard' , pathMatch:'full'},
+  {path:'dashboard',component : DashboardComponent},
+  {path: 'electronics' , component : ElectronicComponent , children :[
+    {path : '' , component : LandingComponent},
+    {path : 'mobile' , component : MobileComponent}
+]},
+  {path: 'fashion' , component : FashionComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ElectronicComponent } from './electronic/electronic.component';
+import { MobileComponent } from './electronic/mobile/mobile.component';
+import { FashionComponent } from './fashion/fashion.component';
+import { LandingComponent } from './electronic/landing/landing.component';
+import { ListComponent } from './electronic/list/list.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DashboardComponent,
+    ElectronicComponent,
+    MobileComponent,
+    FashionComponent,
+    LandingComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
