35d2c7ff0fb992db9fc5c667831834c55dd4866f
manishbalodi <manish.balodi.1998@gmail.com>
2021-09-05 01:48:38 +0530

    cart functionality

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -11,11 +11,33 @@ export class CartComponent implements OnInit {
 
   public cartData : Details[] = [];
   isEmpty : boolean = true;
+  public itemsArray : any;
+  itemCountIncart : number = 0;
 
   constructor(private cartService : CartService) { }
 
   ngOnInit(): void {
     this.cartData = this.cartService.getCartData();
+    // this.cartService.getCartData().subscribe(
+    //   items =>{
+    //     let arr2 : object[] = [];
+    //            items.forEach(element => {
+    //                 arr2.push(element[1]);
+    //            });
+    //           this.itemsArray = arr2;
+    //   }
+    // );
+    //   this.cartData = this.itemsArray;
+    this.itemCountIncart = this.cartData.length;
+    if(this.cartData.length===0)
+      this.isEmpty=false;
+  }
+
+  onRemove(itemId : number)
+  {
+    this.cartService.removeFromCart(itemId);
+    this.cartData = this.cartService.getCartData();
+    this.itemCountIncart = this.cartData.length;
     if(this.cartData.length===0)
       this.isEmpty=false;
   }
--- src/app/service/cart.service.ts
+++ src/app/service/cart.service.ts
@@ -1,30 +1,36 @@
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { map } from "rxjs/operators";
 import { Details } from "../shared/details.model";
 
 @Injectable({providedIn:"root"})
 export class CartService 
 {
-    public cartData : Details[] = [{
-        name: "Mobile Name",
-        features: {
-          feature1: "Feature 1",
-          feature2: "Feature 2",
-          feature3: "Feature 3",
-          feature4: "Feature 4",
-          feature5: "Feature 5",
-        },
-        imagePath: "",
-        price: 10000,
-        description: "",
-      }]
+    public cartData : Details[] = []
+
+    constructor(private http : HttpClient ){}
 
       getCartData()
       {
-          return this.cartData.slice();
+         return this.cartData.slice();
+    //      return  this.http.get<Details[]>('https://shopit-3f3c8-default-rtdb.firebaseio.com/cart.json').
+    //    pipe(map(details=>{
+    //     return Object.entries(details);
+    //    }));
       }
 
       addToCart(item : Details)
       {
         this.cartData.push(item);
+    //     this.http.post('https://shopit-3f3c8-default-rtdb.firebaseio.com/cart.json' , item)
+    //     .subscribe(
+    //   responseData =>{
+    //     //console.log(responseData);
+    //   });
+      }
+
+      removeFromCart(itemId : number)
+      {
+        this.cartData.splice(itemId,1);
       }
 }
